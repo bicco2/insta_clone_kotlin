2b79bceffe2e659540e96283b54ef526d66c0b2d
jinhobyeong <jhb_0921@naver.com>
2022-01-31 22:16:22 +0900

    Initial commit

---
+++ app/src/main/java/com/example/howlstagram_f16/LoginActivity.kt
@@ -0,0 +1,117 @@
+package com.example.howlstagram_f16
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.howlstagram_f16.databinding.ActivityLoginBinding
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+
+class LoginActivity : AppCompatActivity() {
+    var auth : FirebaseAuth? = null
+    var googleSignInClient : GoogleSignInClient? = null
+    var GOOGLE_LOGIN_CODE = 9001
+
+    private var mBinding: ActivityLoginBinding? = null
+    private val binding get() = mBinding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mBinding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+        binding.emailLoginButton.setOnClickListener(){
+            signinAndSignup()
+        }
+
+        binding.googleSignInButton.setOnClickListener(){
+            //first step
+            googleLogin()
+        }
+
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("944860535526-k6fqi3vbffq4t60j27n80hgp54c544r8.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+    }
+
+
+
+    fun googleLogin(){
+        var signInIntent = googleSignInClient?.signInIntent
+        startActivityForResult(signInIntent, GOOGLE_LOGIN_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == GOOGLE_LOGIN_CODE) {
+            if(data != null) {
+                var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+                if (result != null) {
+                    if (result.isSuccess) {
+                        var account = result.signInAccount
+                        firebaseAuthWithGoogle(account)
+                    }
+                }
+            }
+        }
+    }
+    fun firebaseAuthWithGoogle(account: GoogleSignInAccount?) {
+        var credential = GoogleAuthProvider.getCredential(account?.idToken,null)
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener {
+                    task ->
+                if(task.isSuccessful) {
+                    moveMainPage(task.result?.user)
+                }
+                else{
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                }
+
+            }
+    }
+
+    fun signinAndSignup() {
+        auth?.createUserWithEmailAndPassword(binding.emailEdittext.text.toString(),binding.passwordEdittext.text.toString())
+            ?.addOnCompleteListener {
+                task ->
+                    if (task.isSuccessful) {
+                       moveMainPage(task.result?.user)
+                    } else if (task.exception?.message.isNullOrEmpty()) {
+                        Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                    } else {
+                        signinEmail()
+                    }
+            }
+    }
+    fun signinEmail(){
+        auth?.signInWithEmailAndPassword(binding.emailEdittext.text.toString(),binding.passwordEdittext.text.toString())
+            ?.addOnCompleteListener {
+            task ->
+                if(task.isSuccessful) {
+                    moveMainPage(task.result?.user)
+                }
+                else{
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                }
+
+        }
+    }
+
+    fun moveMainPage(user: FirebaseUser?){
+        if(user != null){
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+}
