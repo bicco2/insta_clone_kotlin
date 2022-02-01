641f08f562a1ede805814a99f84a2d7ba648516e
jinhobyeong <jhb_0921@naver.com>
2022-02-01 23:48:41 +0900

    navigation_1

--- app/src/main/java/com/example/howlstagram_f16/MainActivity.kt
+++ app/src/main/java/com/example/howlstagram_f16/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.howlstagram_f16
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        TODO("Not yet implemented")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
+
+
+
 }
