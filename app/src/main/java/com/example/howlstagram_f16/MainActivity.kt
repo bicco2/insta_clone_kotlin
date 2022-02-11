0e1fabfa81cd6e4a08e3b3bbaa7f3e2feecab051
jinhobyeong <jhb_0921@naver.com>
2022-02-11 23:59:16 +0900

    add detailPage

--- app/src/main/java/com/example/howlstagram_f16/MainActivity.kt
+++ app/src/main/java/com/example/howlstagram_f16/MainActivity.kt
@@ -61,6 +61,8 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         binding.bottomNavigation.setOnNavigationItemSelectedListener(this)
         ActivityCompat.requestPermissions(this,arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),1)
 
+
+        binding.bottomNavigation.selectedItemId = R.id.action_home
     }
 
 
--- app/src/main/java/com/example/howlstagram_f16/navigation/DetailViewFragment.kt
+++ app/src/main/java/com/example/howlstagram_f16/navigation/DetailViewFragment.kt
@@ -4,16 +4,73 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.howlstagram_f16.R
+import com.example.howlstagram_f16.databinding.ActivityAddPhotoBinding
+import com.example.howlstagram_f16.databinding.ActivityMainBinding
+import com.example.howlstagram_f16.databinding.FragmentDetailBinding
+import com.example.howlstagram_f16.databinding.ItemDetailBinding
+import com.example.howlstagram_f16.navigation.model.ContentDTO
+import com.google.firebase.firestore.FirebaseFirestore
 
 class DetailViewFragment :Fragment(){
+    var firestore : FirebaseFirestore? = null
+
+    private var mBinding: ItemDetailBinding? = null
+    private val binding get() = mBinding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val binding = ItemDetailBinding.inflate(inflater, container, false)
+
+
         var view = LayoutInflater.from(activity).inflate(R.layout.fragment_detail,container,false)
+        firestore = FirebaseFirestore.getInstance()
         return view
+
+    }
+    inner class DetailViewRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+        var contentDTOs : ArrayList<ContentDTO> = arrayListOf()
+        var contentUidList : ArrayList<String> = arrayListOf()
+
+        init {
+            firestore?.collection("images")?.orderBy("timestamp")?.addSnapshotListener{querySnapshot, firebaseFirestoreException ->
+                contentDTOs.clear()
+                contentUidList.clear()
+                for(snapshot in querySnapshot!!.documents){
+                    var item = snapshot.toObject(ContentDTO::class.java)
+                    contentDTOs.add(item!!)
+                    contentUidList.add(snapshot.id)
+                }
+                notifyDataSetChanged()
+            }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            var view = LayoutInflater.from(parent.context).inflate(R.layout.item_detail,parent,false)
+            return CustomViewHolder(view)
+        }
+
+        inner class CustomViewHolder(view: View) : RecyclerView.ViewHolder(view)
+
+        override fun getItemCount(): Int {
+            return contentDTOs.size
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            var viewholder = (holder as CustomViewHolder).itemView
+
+            //UserId
+
+
+
+        }
+
     }
 }
